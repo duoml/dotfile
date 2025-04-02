cdfc8cbdd5ab151e9bbd594e631a5249857c57ee
duomingliang <duomingliang1@huawei.com>
2025-04-02 17:13:11 +0800

    update

--- install.sh
+++ install.sh
@@ -43,6 +43,9 @@ git clone https://github.com/duoml/NvimConfig.git ~/.config/nvim
 # run nvim and :MasonInstallAll
 
 # installing zsh plugins
+rm -rf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+rm -rf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+rm -rf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
